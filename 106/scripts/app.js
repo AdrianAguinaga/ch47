7b32b3e2357f559039f05d3ea75f70a9c117c8e3
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2024-05-01 19:37:30 -0700

    vs1

--- 106/scripts/app.js
+++ 106/scripts/app.js
@@ -1,1 +1,22 @@
-console.log("hello there");
+function sayHello()
+{
+    console.log("Hello there");
+    
+}
+function sayGoodBye(name,lastName){
+    console.log("Adios! "+name+" "+lastName);
+    var color = "blue";
+}
+
+function init()
+{
+    console.log("hello world");
+    // if the user pays they can visit
+    // {
+    sayHello();
+    let x = "adrian";
+    sayGoodBye(x,"Aguinaga");
+    // }
+}
+
+window.onload = init;//avoid the init()
